807d6cc0253c1548b2ea6252f4aba03d2e0969f7
SAURAV KUMAR SHARMA <162849971+sk7121@users.noreply.github.com>
2026-09-19 19:12:35 +0530

    Time: 0 ms (100%), Space: 10 MB (34.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-            while (i < j && !(s[i] >= 'A' && s[i] <= 'Z') &&
-                   !(s[i] >= 'a' && s[i] <= 'z') &&
-                   !(s[i] >= '0' && s[i] <= '9')) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            while (i < j && !isalnum(s[i]))
                 i++;
-            }
 
-            while (i < j && !(s[j] >= 'A' && s[j] <= 'Z') &&
-                   !(s[j] >= 'a' && s[j] <= 'z') &&
-                   !(s[j] >= '0' && s[j] <= '9')) {
+            while (i < j && !isalnum(s[j]))
                 j--;
-            }
 
-            if (s[i] >= 'A' && s[i] <= 'Z')
-                s[i] = s[i] + 32;
-
-            if (s[j] >= 'A' && s[j] <= 'Z')
-                s[j] = s[j] + 32;
-
-            if (s[i] != s[j])
+            if (tolower(s[i]) != tolower(s[j]))
                 return false;
+
+            i++;
+            j--;
         }
 
         return true;
